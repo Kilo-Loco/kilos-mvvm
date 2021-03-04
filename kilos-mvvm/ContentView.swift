2f817fd8b8b47a24408688d2f2acf3c7f372df55
Kilo Loco <kyle@kiloloco.com>
2021-03-04 11:07:28 -0800

    Initial Commit

---
+++ kilos-mvvm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  kilos-mvvm
+//
+//  Created by Kilo Loco on 3/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ kilos-mvvm/kilos_mvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  kilos_mvvmApp.swift
+//  kilos-mvvm
+//
+//  Created by Kilo Loco on 3/4/21.
+//
+
+import SwiftUI
+
+@main
+struct kilos_mvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
